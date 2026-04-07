706cdc5b21d74c315a71f83f1a4d480b930889d4
David Oyinbo <dev.davexoyinbo@gmail.com>
2026-04-07 13:20:33 +0100

    test: builder

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -9,6 +9,7 @@ mod test_absolute_hybrid_rate_limiter;
 mod test_absolute_local_rate_limiter;
 #[cfg(any(feature = "redis-tokio", feature = "redis-smol"))]
 mod test_absolute_redis_rate_limiter;
+mod test_builder;
 mod test_cleanup_loop;
 mod test_common_validation;
 #[cfg(any(feature = "redis-tokio", feature = "redis-smol"))]
